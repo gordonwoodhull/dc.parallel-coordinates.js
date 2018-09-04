b0e72bbd28242dd72b43a4a08caeea5471d6780d
Gordon Woodhull <gordon@woodhull.com>
2018-09-04 18:16:46 -0400

    add pie chart

--- web/cars.js
+++ web/cars.js
@@ -1,11 +1,29 @@
 d3.csv('cars.csv').then(function(data) {
+    var numeric = ['cylinders','displacement (cc)','power (hp)','weight (lb)','0-60 mph (s)','year'];
+    data.forEach(function(d) {
+        numeric.forEach(function(f) {
+            d[f] = +d[f];
+        });
+    });
+
     var cf = crossfilter(data);
     var economyDim = cf.dimension(d => d['economy (mpg)']);
 
-    var pc = dc_parcoords.parallelCoords('#parcoords')
+    var par = dc_parcoords.parallelCoords('#parcoords');
+    par
         .width(600)
         .height(300)
         .dimensions([economyDim]);
+
+    var yearDim = cf.dimension(d => d.year),
+        yearWeightGroup = yearDim.group().reduceSum(d => d['weight (lb)']);
+
+    var pie = dc.pieChart('#pie');
+    pie
+        .width(250)
+        .height(250)
+        .dimension(yearDim)
+        .group(yearWeightGroup);
 
     dc.renderAll();
 });
